930da64cb392e5ecc1d6d8d3633d780252540af5
Tom <tom@contextfree.info>
2021-11-12 19:57:37 -0700

    Convert rest of d api following patterns

--- src/wasm4.d
+++ src/wasm4.d
@@ -8,7 +8,7 @@ extern(C):
 // │                                                                           │
 // └───────────────────────────────────────────────────────────────────────────┘
 
-enum ScreenSize = 160;
+enum screenSize = 160;
 
 // ┌───────────────────────────────────────────────────────────────────────────┐
 // │                                                                           │
@@ -16,106 +16,97 @@ enum ScreenSize = 160;
 // │                                                                           │
 // └───────────────────────────────────────────────────────────────────────────┘
 
-// #define PALETTE ((uint32_t*)0x04)
+enum palette = cast(uint*)0x04;
 enum drawColors = cast(ushort*)0x14;
 enum gamepad1 = cast(const ubyte*)0x16;
-// #define GAMEPAD2 ((const uint8_t*)0x17)
-// #define GAMEPAD3 ((const uint8_t*)0x18)
-// #define GAMEPAD4 ((const uint8_t*)0x19)
-// #define MOUSE_X ((const int16_t*)0x1a)
-// #define MOUSE_Y ((const int16_t*)0x1c)
-// #define MOUSE_BUTTONS ((const uint8_t*)0x1e)
-// #define FRAMEBUFFER ((uint8_t*)0xa0)
+enum gamepad2 = cast(const ubyte*)0x17;
+enum gamepad3 = cast(const ubyte*)0x18;
+enum gamepad4 = cast(const ubyte*)0x19;
+enum mouseX = cast(const short*)0x1a;
+enum mouseY = cast(const short*)0x1c;
+enum mouseButtons = cast(const ubyte*)0x1e;
+enum framebuffer = cast(ubyte*)0xa0;
 
 enum button1 = 1;
-// #define BUTTON_2 2
-// #define BUTTON_ENTER 4
-// #define BUTTON_UNUSED 8
-// #define BUTTON_LEFT 16
-// #define BUTTON_RIGHT 32
-// #define BUTTON_UP 64
-// #define BUTTON_DOWN 128
+enum button2 = 2;
+enum buttonEnter = 4;
+enum buttonUnused = 8;
+enum buttonLeft = 16;
+enum buttonRight = 32;
+enum buttonUp = 64;
+enum buttonDown = 128;
 
-// // ┌───────────────────────────────────────────────────────────────────────────┐
-// // │                                                                           │
-// // │ Drawing Functions                                                         │
-// // │                                                                           │
-// // └───────────────────────────────────────────────────────────────────────────┘
+// ┌───────────────────────────────────────────────────────────────────────────┐
+// │                                                                           │
+// │ Drawing Functions                                                         │
+// │                                                                           │
+// └───────────────────────────────────────────────────────────────────────────┘
 
-// /** Copies pixels to the framebuffer. */
-// WASM_IMPORT("blit")
+/** Copies pixels to the framebuffer. */
 void blit(const ubyte* data, int x, int y, uint width, uint height, uint flags);
 
-// /** Copies a subregion within a larger sprite atlas to the framebuffer. */
-// WASM_IMPORT("blitSub")
-// void blitSub (const uint8_t* data, int32_t x, int32_t y, uint32_t width, uint32_t height,
-//     uint32_t srcX, uint32_t srcY, uint32_t stride, uint32_t flags);
+/** Copies a subregion within a larger sprite atlas to the framebuffer. */
+void blitSub(
+    const ubyte* data, int x, int y, uint width, uint height,
+    uint srcX, uint srcY, uint stride, uint flags,
+);
 
-// #define BLIT_2BPP 1
+enum blit2Bpp = 1;
 enum blit1Bpp = 0;
-// #define BLIT_FLIP_X 2
-// #define BLIT_FLIP_Y 4
-// #define BLIT_ROTATE 8
+enum blitFlipX = 2;
+enum blitFlipY = 4;
+enum blitRotate = 8;
 
-// /** Draws a line between two points. */
-// WASM_IMPORT("line")
-// void line (int32_t x, int32_t y, uint32_t width, uint32_t height);
+/** Draws a line between two points. */
+void line(int x, int y, uint width, uint height);
 
-// /** Draws a horizontal line. */
-// WASM_IMPORT("hline")
-// void hline (int32_t x, int32_t y, uint32_t len);
+/** Draws a horizontal line. */
+void hline(int x, int y, uint len);
 
-// /** Draws a vertical line. */
-// WASM_IMPORT("vline")
-// void vline (int32_t x, int32_t y, uint32_t len);
+/** Draws a vertical line. */
+void vline(int x, int y, uint len);
 
-// /** Draws an oval (or circle). */
-// WASM_IMPORT("oval")
-// void oval (int32_t x, int32_t y, uint32_t width, uint32_t height);
+/** Draws an oval (or circle). */
+void oval(int x, int y, uint width, uint height);
 
-// /** Draws a rectangle. */
-// WASM_IMPORT("rect")
-// void rect (int32_t x, int32_t y, uint32_t width, uint32_t height);
+/** Draws a rectangle. */
+void rect(int x, int y, uint width, uint height);
 
 // /** Draws text using the built-in system font. */
 void text(const char* text, int x, int y);
 
-// // ┌───────────────────────────────────────────────────────────────────────────┐
-// // │                                                                           │
-// // │ Sound Functions                                                           │
-// // │                                                                           │
-// // └───────────────────────────────────────────────────────────────────────────┘
+// ┌───────────────────────────────────────────────────────────────────────────┐
+// │                                                                           │
+// │ Sound Functions                                                           │
+// │                                                                           │
+// └───────────────────────────────────────────────────────────────────────────┘
 
-// /** Plays a sound tone. */
-// WASM_IMPORT("tone")
-// void tone (uint32_t frequency, uint32_t duration, uint32_t volume, uint32_t flags);
+/** Plays a sound tone. */
+void tone(uint frequency, uint duration, uint volume, uint flags);
 
-// #define TONE_PULSE1 0
-// #define TONE_PULSE2 1
-// #define TONE_TRIANGLE 2
-// #define TONE_NOISE 3
-// #define TONE_MODE1 0
-// #define TONE_MODE2 4
-// #define TONE_MODE3 8
-// #define TONE_MODE4 12
+enum TONE_PULSE1 = 0;
+enum TONE_PULSE2 = 1;
+enum TONE_TRIANGLE = 2;
+enum TONE_NOISE = 3;
+enum TONE_MODE1 = 0;
+enum TONE_MODE2 = 4;
+enum TONE_MODE3 = 8;
+enum TONE_MODE4 = 12;
 
-// // ┌───────────────────────────────────────────────────────────────────────────┐
-// // │                                                                           │
-// // │ Storage Functions                                                         │
-// // │                                                                           │
-// // └───────────────────────────────────────────────────────────────────────────┘
+// ┌───────────────────────────────────────────────────────────────────────────┐
+// │                                                                           │
+// │ Storage Functions                                                         │
+// │                                                                           │
+// └───────────────────────────────────────────────────────────────────────────┘
 
-// /** Reads up to `size` bytes from persistent storage into the pointer `destPtr`. */
-// WASM_IMPORT("diskr")
-// uint32_t diskr (void* dest, uint32_t size);
+/** Reads up to `size` bytes from persistent storage into the pointer `destPtr`. */
+uint diskr(void* dest, uint size);
 
-// /** Writes up to `size` bytes from the pointer `srcPtr` into persistent storage. */
-// WASM_IMPORT("diskw")
-// uint32_t diskw (const void* src, uint32_t size);
+/** Writes up to `size` bytes from the pointer `srcPtr` into persistent storage. */
+uint diskw(const void* src, uint size);
 
-// /** Prints a message to the debug console. */
-// WASM_IMPORT("trace") void trace (const char* str);
+/** Prints a message to the debug console. */
+void trace(const char* str);
 
-// /** Prints a message to the debug console. */
-// __attribute__((__format__ (__printf__, 1, 2)))
-// WASM_IMPORT("tracef") void tracef (const char* fmt, ...);
+/** Prints a message to the debug console. */
+void tracef(const char* fmt, ...);
